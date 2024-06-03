83c9bae2a452c9493a71c9d031c3f7813db7b017
PLANEJAMENTO\erica.cunha <erica.silva@planejamento.rr.gov.br>
2024-06-03 13:15:11 -0400

    Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:app_criptu_moedas/repositories/conta_repository.dart';
+import 'package:app_criptu_moedas/services/auth_service.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-import 'configs/hive_config.dart';
+import 'firebase_options.dart';
 import 'repositories/favoritas_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'configs/app_settings.dart';
 import 'meu_aplicativo.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await HiveConfig.start();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     MultiProvider(
       providers: [
+        ChangeNotifierProvider(create: (context) => AuthService()),
         ChangeNotifierProvider(create: (context) => ContaRepository()),
         ChangeNotifierProvider(create: (context) => AppSettings()),
         ChangeNotifierProvider(create: (context) => FavoritasRepository()),
